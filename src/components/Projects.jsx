f50014606def51b10c9c83b712575aeba1784c1d
Jabali Muriithi <you@example.com>
2026-03-05 16:37:17 -0600

    bug fixes: 404 error on main.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ function Projects() {
     <div className="projects">
       <h3>My Projects:</h3>
       <div className="project-card">
-        <h4>Me-Good Health App</h4>
+        <h4>Me-Good Fitness Tracker App</h4>
         <section className="project-content">
           <img src='./megooddashboard.png' alt="Project 1" className="project-img"/>
           <p>A patient-facing wellness platform built using React and AWS serverless architecture, with real-time calorie balance tracking, daily steps and live heart rate monitoring. Exposed me to cloud-based authentification flows, distributed systems, and scalable data storage. You can read more about it <a href="https://github.com/jb2dawrrld/MeGood"> here. </a></p>
